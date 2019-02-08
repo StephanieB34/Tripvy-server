7bd14af206698f3c416b85aa9305e65c7cf24268
Sebastian Bastidas <sbastidasr@gmail.com>
2019-02-08 11:47:53 -0500

    Session

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-var button = $("button")
+var button = $("button");
 
-$("#log-in").on("click", function () {
-    $("#landing-page").hide();
-    $("#signup-page").hide();
-    $("#projects-page").hide();
-    $("#login-page").show();
-    $(".main-section").hide();
-    $(".section").hide();
+$("#log-in").on("click", function() {
+  $("#landing-page").hide();
+  $("#signup-page").hide();
+  $("#projects-page").hide();
+  $("#login-page").show();
+  $(".main-section").hide();
+  $(".section").hide();
 });
 
 $("#enter").on("click", function() {
-    $("#landing-page").hide();
-    $("#signup-page").hide();
-    $("#login-page").hide();
-    $("#projects-page").show();
-    $(".main-section").hide();
-    $(".section").hide();
+  $("#landing-page").hide();
+  $("#signup-page").hide();
+  $("#login-page").hide();
+  $("#projects-page").show();
+  $(".main-section").hide();
+  $(".section").hide();
 });
 
-$("#register").on("click", function () {
-    $("#landing-page").hide();
-    $("#login-page").hide();
-    $("#projects-page").hide();
-    $("#signup-page").show();
-    $(".main-section").hide();
-    $(".section").hide();
+$("#register").on("click", function() {
+  $("#landing-page").hide();
+  $("#login-page").hide();
+  $("#projects-page").hide();
+  $("#signup-page").show();
+  $(".main-section").hide();
+  $(".section").hide();
 });
 
-$("#sign-up").on("click", function () {
-    $("#landing-page").hide();
-    $("#login-page").hide();
-    $("#signup-page").hide();
-    $("#projects-page").show();
-    $(".main-section").hide();
-    $(".section").hide();
+$("#sign-up").on("click", function() {
+  $("#landing-page").hide();
+  $("#login-page").hide();
+  $("#signup-page").hide();
+  $("#projects-page").show();
+  $(".main-section").hide();
+  $(".section").hide();
 });
 
+$(".login-form").on("submit", function(e) {
+  e.preventDefault();
+  $("#landing-page").hide();
+  $("#login-page").hide();
+  $("#signup-page").hide();
+  $("#projects-page").show();
+  $(".main-section").hide();
+  $(".section").hide();
+});
