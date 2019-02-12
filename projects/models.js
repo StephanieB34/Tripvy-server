5580e4037afca8c7b763eb22c5e69a681b120ae1
Sebastian Bastidas <sbastidasr@gmail.com>
2019-02-12 11:51:55 -0500

    Server

---
+++ projects/models.js
@@ -0,0 +1,41 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+const ProjectSchema = mongoose.Schema({
+  projectName: {
+    type: String,
+    required: true
+  },
+  budget: {
+    type: Number,
+    default: 0
+  },
+  materialsNeeded: {
+    type: String,
+    default: ""
+  },
+  startDate: {
+    type: String,
+    required: true
+  },
+  endDate: {
+    type: String
+  }
+});
+
+ProjectSchema.methods.serialize = function() {
+  return {
+    projectName: this.projectName || "",
+    budget: this.budget || "",
+    materialsNeeded: this.materialsNeeded || "",
+    startDate: this.startDate || "",
+    endDate: this.endDate || ""
+  };
+};
+
+const Project = mongoose.model("Project", ProjectSchema);
+
+module.exports = { Project };
---
+++ projects/router.js
@@ -0,0 +1,87 @@
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { Project } = require("./models");
+
+const router = express.Router();
+
+const jsonParser = bodyParser.json();
+
+router.get("/", (req, res) => {
+  Project.find()
+    .then(projects => {
+      res.json(projects.map(project => project.serialize()));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "something went terribly wrong" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Project.findById(req.params.id)
+    .then(project => res.json(project.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "something went horribly awry" });
+    });
+});
+
+// router.post("/projects", jsonParser, (req, res) => {
+// const requiredFields = ['projectName', 'startDate', 'budget', 'materialsNeeded', 'endDate'];
+//   for (let i = 0; i < requiredFields.length; i++) {
+//     const field = requiredFields[i];
+//     if (!(field in req.body)) {
+//       const message = `Missing \`${field}\` in request body`;
+//       console.error(message);
+//       return res.status(400).send(message);
+//     }
+//   }
+
+//   BlogPost.create({
+//    projectName: req.body.projectName,
+// startDate: req.body.startDate,
+// budget: req.body.budget,
+// materialsNeeded: req.body.materialsNeeded,
+// endDate: req.body.endDate
+//   })
+//     .then(blogPost => res.status(201).json(blogPost.serialize()))
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({ error: "Something went wrong" });
+//     });
+// });
+
+// router.delete("/posts/:id", (req, res) => {
+//   BlogPost.findByIdAndRemove(req.params.id)
+//     .then(() => {
+//       res.status(204).json({ message: "success" });
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({ error: "something went terribly wrong" });
+//     });
+// });
+
+// router.put("/posts/:id", jsonParser, (req, res) => {
+//   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+//     res.status(400).json({
+//       error: "Request path id and request body id values must match"
+//     });
+//   }
+
+//   const updated = {};
+// const updatableFields = ['projectName', 'startDate', 'budget', 'materialsNeeded', 'endDate'];
+//   updateableFields.forEach(field => {
+//     if (field in req.body) {
+//       updated[field] = req.body[field];
+//     }
+//   });
+
+//   BlogPost.findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+//     .then(updatedPost => res.status(204).end())
+//     .catch(err => res.status(500).json({ message: "Something went wrong" }));
+// });
+
+module.exports = { router };
